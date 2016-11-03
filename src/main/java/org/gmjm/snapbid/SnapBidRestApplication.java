269f8e98a218fbbedbc30d2155767af5e3923b08
Andy Glassman <aglassman@digitalmeasures.com>
2016-11-02 23:50:55 -0500

    Removed security for now.

--- src/main/java/org/gmjm/snapbid/SnapBidRestApplication.java
+++ src/main/java/org/gmjm/snapbid/SnapBidRestApplication.java
@@ -1,42 +1,18 @@
 package org.gmjm.snapbid;
 
-import java.security.Principal;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @SpringBootApplication
-@EnableOAuth2Sso
 @RestController
-public class SnapBidRestApplication extends WebSecurityConfigurerAdapter{
+public class SnapBidRestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SnapBidRestApplication.class, args);
 	}
 
-	@RequestMapping("/user")
-	public Principal user(Principal principal) {
-		return principal;
-	}
 
-
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http
-			.authorizeRequests()
-				.antMatchers("/**")
-				.authenticated();
-
-
-		http.csrf().disable();
-
-	}
 
 }
--- src/main/java/org/gmjm/snapbid/web/WebsocketController.java
+++ src/main/java/org/gmjm/snapbid/web/WebsocketController.java
@@ -1,16 +1,9 @@
 package org.gmjm.snapbid.web;
 
-import java.security.Principal;
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -22,11 +15,9 @@ public class WebsocketController
 
 	@MessageMapping("/chat")
 	@SendTo("/topic/chat")
-	public String greeting(Principal principal, String message) throws Exception {
+	public String greeting(String message) throws Exception {
 
-		Map<String,String> details = (Map)((OAuth2Authentication)principal).getUserAuthentication().getDetails();
-
-		return details.get("name") + " : " + message;
+		return message;
 	}
 
 }
